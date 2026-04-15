50c319940043ddcba32e2d1087b016735faacfa1
soyamagu <soyamagu@student.42tokyo.jp>
2026-04-15 20:27:49 +0900

    [add] new file: sort.c

---
+++ sort.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: soyamagu <soyamagu@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/13 20:31:00 by soyamagu          #+#    #+#             */
+/*   Updated: 2026/04/15 20:27:16 by soyamagu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+// void	ft_radix_sort(t_list **stack_a, t_list **stack_b)
+// {
+// 	max = bit桁
+// 	i = 0;
+// 	j = 0;
+// 	c = 0;
+// 	size_a = ft_lst_size(stack_a);
+
+// while (i < max)
+// {
+// 	c = 0;  // ← 必須
+
+// 	while (c < size_a)
+// 	{
+// 		if (((*stack_a)->data >> i) & 1)
+// 			ra;
+// 		else
+// 			pb;
+// 		c++;
+// 	}
+
+// 	j = 0;  // ← 必須
+// 	while (j < size_b)
+// 	{
+// 		pa;
+// 		j++;
+// 	}
+
+// 	size_a = ft_lst_size(stack_a);
+// 	size_b = ft_lst_size(stack_b);
+// 	i++;
+// }
+// }
+
+// void	sort_lst(t_list **stack_a, t_list **stack_b)
+// {
+// 	//2
+
+// 	//3
+
+// 	//4
+
+// 	//5
+
+// 	//6~
+// }
